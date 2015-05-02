cbabea1f22b98170bbf75232b5a737c02deddc27
stonedmind <stonrmind@gmail.com>
2015-05-02 15:06:00 +0530

    small changes

--- python/image.py
+++ python/image.py
@@ -1,50 +1,97 @@
 import numpy as np
 import cv2
+import sys
+import lcd
+
+# LCD Pins
+LCD_RS 			= 7
+LCD_E  			= 8
+LCD_D4 			= 25
+LCD_D5 			= 24
+LCD_D6 			= 23
+LCD_D7 			= 18
+
+# GPIO Pins
+SWITCH_CAPTURE 	= 11
+SWITCH_STOP		= 9
 
 cv2.namedWindow('result')
-
-def on_mouse(event, x, y, flag, param):
-	print str(x) + ',' + str(y)
 
 _redL = np.array([1,150,60])
 _redH = np.array([33,255,255])
 
+def gpioInit():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LED_GREEN, GPIO.OUT)  
+	GPIO.setup(LED_RED, GPIO.OUT) 
+	GPIO.setup(SWITCH_CAPTURE, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+	GPIO.setup(SWITCH_STOP, GPIO.IN, pull_up_down = GPIO.PUD_UP) 
+	GPIO.output(LED_RED, True)
+	GPIO.output(LED_GREEN, True)
 
-frame 	= cv2.imread("1.jpg",-1)
-frame	= cv2.resize(frame, (640,480))
-frame 	= cv2.GaussianBlur(frame, (5,5),0)
-hsv 	= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-mask	= cv2.inRange(hsv,_redL,_redH)
+def run():
+	_frame 	= cv2.imread("1.jpg",-1)
+	frame 	= np.zeros((640,480,3), np.uint8)
+	frame	= cv2.resize(frame, (640,480))
+	frame 	= cv2.GaussianBlur(frame, (5,5),0)
+	hsv 	= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	mask	= cv2.inRange(hsv,_redL,_redH)
 
-se1 	= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
-se2 	= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
+	se1 	= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
+	se2 	= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
 
-kernel 	= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-mask 	= cv2.dilate(mask,kernel,iterations = 1)
+	kernel 	= cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+	mask 	= cv2.dilate(mask,kernel,iterations = 1)
 
-mask 	= cv2.morphologyEx(mask, cv2.MORPH_CLOSE, se1)
-mask 	= cv2.morphologyEx(mask, cv2.MORPH_OPEN, se2)
+	mask 	= cv2.morphologyEx(mask, cv2.MORPH_CLOSE, se1)
+	mask 	= cv2.morphologyEx(mask, cv2.MORPH_OPEN, se2)
 
-moments = cv2.moments(mask)
-area	= moments['m00']
-__x		= moments['m10']
-__y		= moments['m01']
+	moments = cv2.moments(mask)
+	area	= moments['m00']
+	__x		= moments['m10']
+	__y		= moments['m01']
 
-X 		= __x / area
-Y 		= __y / area
+	X 		= __x / area
+	Y 		= __y / area
 
-print "X : " + str(X)
-print "Y : " + str(Y)
-print "A : " + str(area)
+	print "X : " + str(X)
+	print "Y : " + str(Y)
+	print "A : " + str(area)
 
-# cv2.cv.SetMouseCallback("result",on_mouse)
+	if area > 2500000:
+		cv2.circle(frame,(int(X),int(Y)), int((area / 100000)), (22,68,196), 2)
+		lcd.lcd_byte(0x94, False)
+		lcd.lcd_string("Loc(x,y): ")
+		lcd.lcd_string(str(int(X)) + ", " + str(int(Y)))
+		lcd.lcd_string("    ")
+		lcd.lcd_byte(0xD4, False)
+		lcd.lcd_string("radius: ")
+		lcd.lcd_string(str(int((area / 100000))))
+		lcd.lcd_string("    ")
+	cv2.imshow("result",frame)
 
-if area > 2500000:
-	cv2.circle(frame,(int(X),int(Y)), int((area / 100000)), (22,68,196), 2)
+# Init servo
+os.system("servod --p1pins=7,0,0,0,0,0,0,0")
+os.system("echo 0=50% > /dev/servoblaster")
 
-cv2.imshow("result",frame)
+# Init lcd
+lcd = LCD(LCD_RS, LCD_E, LCD_D4, LCD_D5,LCD_D6,LCD_D7)
+lcd.lcd_byte(0x80, False)
+lcd.lcd_string("  Color Detection  ")
 
-k = cv2.waitKey(0)
+while True:
+	if GPIO.input(SWITCH_CAPTURE) 	== 0:
+		lcd.lcd_byte(0xC0, False)
+		lcd.lcd_string("Capturing         ")
+		run()
+		print "Capture"
+	if GPIO.input(SWITCH_STOP) 		== 0:
+		lcd.lcd_string("Clearing          ")
+		print "Clear"
+
+	k = cv.waitKey(5) & 0xFF
+	if k == 27:
+		break
 
 
 
---
+++ python/lcd.py
@@ -0,0 +1,99 @@
+
+import RPi.GPIO as GPIO
+
+class LCD:
+
+	def __init__(self, lcd_rs,lcd_en,lcd_d4,lcd_d5,lcd_d6,lcd_d7):
+
+		self.LCD_RS = lcd_rs
+		self.LCD_EN = lcd_en
+		self.LCD_D4 = lcd_d4
+		self.LCD_D5 = lcd_d5
+		self.LCD_D6 = lcd_d6
+		self.LCD_D7 = lcd_d7
+
+		self.LCD_WIDTH = 20
+		self.LCD_CHR = True
+		self.LCD_CMD = False
+
+		self.LCD_LINE_1 = 0x80
+		self.LCD_LINE_2 = 0xC0
+
+		self.E_PULSE = 0.0001
+		self.E_DELAY = 0.0001
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.LCD_EN, GPIO.OUT)  # E
+		GPIO.setup(self.LCD_RS, GPIO.OUT) # RS
+		GPIO.setup(self.LCD_D4, GPIO.OUT) # DB4
+		GPIO.setup(self.LCD_D5, GPIO.OUT) # DB5
+		GPIO.setup(self.LCD_D6, GPIO.OUT) # DB6
+		GPIO.setup(self.LCD_D7, GPIO.OUT) # DB7
+
+		self.lcd_init()
+
+		# Initialise display
+		self.lcd_init()
+
+	def lcd_init(self):
+		# Initialise display
+		self.lcd_byte(0x33,False)
+		self.lcd_byte(0x32,False)
+		self.lcd_byte(0x28,False)
+		self.lcd_byte(0x0C,False)
+		self.lcd_byte(0x06,False)
+		self.lcd_byte(0x01,False)  
+
+	def lcd_string(self, message):
+		# Send string to display
+
+		message = message.ljust(self.LCD_WIDTH," ") 
+
+		for i in range(self.LCD_WIDTH):
+			self.lcd_byte(ord(message[i]),True)
+
+	def lcd_byte(self,bits, mode):
+
+		GPIO.output(self.LCD_RS, mode) # RS
+
+		# High bits
+		GPIO.output(self.LCD_D4, False)
+		GPIO.output(self.LCD_D5, False)
+		GPIO.output(self.LCD_D6, False)
+		GPIO.output(self.LCD_D7, False)
+		if bits&0x10==0x10:
+			GPIO.output(self.LCD_D4, True)
+		if bits&0x20==0x20:
+			GPIO.output(self.LCD_D5, True)
+		if bits&0x40==0x40:
+			GPIO.output(self.LCD_D6, True)
+		if bits&0x80==0x80:
+			GPIO.output(self.LCD_D7, True)
+
+		# Toggle 'Enable' pin
+		time.sleep(self.E_DELAY)
+		GPIO.output(self.LCD_EN, True)
+		time.sleep(self.E_PULSE)
+		GPIO.output(self.LCD_EN, False)
+		time.sleep(self.E_DELAY)     
+
+		# Low bits
+		GPIO.output(self.LCD_D4, False)
+		GPIO.output(self.LCD_D5, False)
+		GPIO.output(self.LCD_D6, False)
+		GPIO.output(self.LCD_D7, False)
+		if bits&0x01==0x01:
+			GPIO.output(self.LCD_D4, True)
+		if bits&0x02==0x02:
+			GPIO.output(self.LCD_D5, True)
+		if bits&0x04==0x04:
+			GPIO.output(self.LCD_D6, True)
+		if bits&0x08==0x08:
+			GPIO.output(self.LCD_D7, True)
+
+		# Toggle 'Enable' pin
+		time.sleep(self.E_DELAY)
+		GPIO.output(self.LCD_EN, True)
+		time.sleep(self.E_PULSE)
+		GPIO.output(self.LCD_EN, False)
+		time.sleep(self.E_DELAY)   
